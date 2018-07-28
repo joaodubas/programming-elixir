3b6a6fe3062c2f2f987bab1d9c923cec88057a25
Joao P Dubas <joao.dubas@gmail.com>
2018-07-28 16:37:40 -0300

    chapter 01

---
+++ ch01/pmap1.exs
@@ -0,0 +1,9 @@
+defmodule Parallel do
+	def pmap(collection, func) do
+		collection
+		|>Enum.map(&(Task.async(fn -> func.(&1)	 end)))
+		|>Enum.map(&Task.await/1)
+	end
+end
+
+# use as Parallel.pmap 1..1000, &(&1 * &1)
