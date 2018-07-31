490c584f6246fc52b20715af2dba599d93487a7f
Joao P Dubas <joao.dubas@gmail.com>
2018-07-31 09:23:47 -0300

    chapter 13: provide github url as config

--- ch13/issues/lib/issues/github_issues.ex
+++ ch13/issues/lib/issues/github_issues.ex
@@ -1,4 +1,5 @@
 defmodule Issues.GithubIssues do
+  @github_url Application.get_env(:issues, :github_url)
   @user_agent [{"User-Agent", "Elixir joao@local"}]
 
   def fetch(user, project) do
@@ -8,7 +9,7 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
