ebfac30d04983826b926d9a68c7869dcae51f3bd
Joao P Dubas <joao.dubas@gmail.com>
2018-07-31 09:07:32 -0300

    chapter 13: convert response into map

--- ch13/issues/lib/issues/github_issues.ex
+++ ch13/issues/lib/issues/github_issues.ex
@@ -11,10 +11,13 @@ defmodule Issues.GithubIssues do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
+  def handle_response({_, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_error(),
+      body        |> Poison.Parser.parse!()
+    }
   end
-  def handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
-  end
+
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_), do: :error
 end
