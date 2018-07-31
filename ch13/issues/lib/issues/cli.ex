e9137e882f1547b003b223a52c3cc1457180a6f4
Joao P Dubas <joao.dubas@gmail.com>
2018-07-31 07:52:50 -0300

    chapter 13: process args to fetch github issues

--- ch13/issues/lib/issues/cli.ex
+++ ch13/issues/lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -22,7 +24,17 @@ defmodule Issues.CLI do
   def parse_args(argv) do
     OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
     |> elem(1)
-    |> args_to_internal_representation()
+    |> args_to_internal_representation
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 
   def args_to_internal_representation([user, project, count]) do
