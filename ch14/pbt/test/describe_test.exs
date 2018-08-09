0f12176d0744e19e131b4549c8ae8468356a4889
Joao P Dubas <joao.dubas@gmail.com>
2018-08-09 20:07:51 -0300

    chapter 14: describe: group integer tests

--- ch14/pbt/test/describe_test.exs
+++ ch14/pbt/test/describe_test.exs
@@ -2,18 +2,20 @@ defmodule StatsTest do
   use ExUnit.Case
   doctest Stats
 
-  test "calculate sum" do
-    l = [1, 3, 5, 7, 9]
-    assert Stats.sum(l) == 25
-  end
+  describe "Stats on list of ints" do
+    test "calculate sum" do
+      l = [1, 3, 5, 7, 9]
+      assert Stats.sum(l) == 25
+    end
 
-  test "calculate count" do
-    l = [1, 3, 5, 7, 9]
-    assert Stats.count(l) == 5
-  end
+    test "calculate count" do
+      l = [1, 3, 5, 7, 9]
+      assert Stats.count(l) == 5
+    end
 
-  test "calculate average" do
-    l = [1, 3, 5, 7, 9]
-    assert Stats.average(l) == 5
+    test "calculate average" do
+      l = [1, 3, 5, 7, 9]
+      assert Stats.average(l) == 5
+    end
   end
 end
