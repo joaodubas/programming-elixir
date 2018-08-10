8f3f956ff5108f578f91e785a29f28c3f8bb0caf
Joao P Dubas <joao.dubas@gmail.com>
2018-08-10 09:27:13 -0300

    chapter 14: property test: add count test

--- ch14/pbt/test/property_test.exs
+++ ch14/pbt/test/property_test.exs
@@ -3,6 +3,12 @@ defmodule StatsPropertyTest do
   use ExUnitProperties
 
   describe "Stats on list of int" do
+    property "count is not negative" do
+      check all l <- list_of(integer()) do
+        assert Stats.count(l) >= 0
+      end
+    end
+
     property "single element lists are their own sum" do
       check all number <- integer() do
         assert Stats.sum([number]) == number
