b2eb0c50afb2ebb292ae2c866371032e5f8a389e
Joao P Dubas <joao.dubas@gmail.com>
2018-08-11 12:58:56 -0300

    chapter 13: add coverage tool

--- ch13/issues/mix.exs
+++ ch13/issues/mix.exs
@@ -10,7 +10,14 @@ defmodule Issues.MixProject do
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       escript: escript_config(),
-      deps: deps()
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ],
+      deps: deps(),
     ]
   end
 
@@ -24,10 +31,11 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.0.0"},
-      {:poison,    "~> 3.1"},
-      {:ex_doc,    "~> 0.18.1"},
-      {:earmark,   "~> 1.2.4"},
+      {:httpoison,   "~> 1.0.0"},
+      {:poison,      "~> 3.1"},
+      {:ex_doc,      "~> 0.18.1"},
+      {:earmark,     "~> 1.2.4", override: true},
+      {:excoveralls, "~> 0.5.5", only: :test},
     ]
   end
 
