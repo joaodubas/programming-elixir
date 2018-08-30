9cdf37b264a94ab54215eb94721fc673dab89f08
Joao P Dubas <joao.dubas@gmail.com>
2018-08-30 08:32:18 -0300

    chapter 15: spawn after pseudo pattern
    Handle timeout if message isn't received.

---
+++ ch15-eg-spawn3.exs
@@ -0,0 +1,25 @@
+defmodule Spawn do
+  def greet() do
+    receive do
+      {sender, msg} ->
+        send(sender, {:ok, "Hello #{msg}"})
+    end
+  end
+end
+
+# here is the client
+pid = spawn(Spawn, :greet, [])
+send(pid, {self(), "joão"})
+receive do
+  {:ok, msg} ->
+    IO.puts(msg)
+end
+
+# NOTE: add pseudo pattern `after`, to avoid for a service that isn't available
+send(pid, {self(), "paulo"})
+receive do
+  {:ok, msg} ->
+    IO.puts(msg)
+  after 500 ->
+    IO.puts("Greeter has gone away")
+end
