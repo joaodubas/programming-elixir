12ccc796eb7a4b2a4ade015a99a404b9804b1e96
Joao P Dubas <joao.dubas@gmail.com>
2018-08-30 23:29:12 -0300

    chapter 15: link: trap failure
    This should be handled by process supervisor.

---
+++ ch15-eg-link3.exs
@@ -0,0 +1,21 @@
+defmodule Link do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep(500)
+    exit(:boom)
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(Link, :sad_function, [])
+    receive do
+      msg ->
+        IO.puts("Message received: #{inspect(msg)}")
+      after 1000 ->
+        IO.puts("Nothing happened as far as I am concerned")
+    end
+  end
+end
+
+Link.run
