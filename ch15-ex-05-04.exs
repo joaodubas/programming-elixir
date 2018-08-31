172a248ee0fb24727cffa76e921799c68d2b718e
Joao P Dubas <joao.dubas@gmail.com>
2018-08-31 07:32:54 -0300

    chapter 15: exercise: monitor process failure

---
+++ ch15-ex-05-04.exs
@@ -0,0 +1,15 @@
+defmodule Anthropometry do
+  def bmi!(_, weight, height) when not is_integer(weight) or not is_integer(height) do
+    raise "BMI can't be calculated with #{weight} and #{height}"
+  end
+
+  def run() do
+    spawn_monitor(Anthropometry, :bmi!, [self(), "100", "168"])
+    receive do
+      msg ->
+        IO.puts(inspect(msg))
+    end
+  end
+end
+
+Anthropometry.run
