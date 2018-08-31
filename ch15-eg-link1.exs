41452ee1d800bf088c922ff054d59e140e210a8c
Joao P Dubas <joao.dubas@gmail.com>
2018-08-30 23:22:31 -0300

    chapter 15: link: process failure

---
+++ ch15-eg-link1.exs
@@ -0,0 +1,20 @@
+defmodule Link do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep(500)
+    exit(:boom)
+  end
+
+  def run do
+    spawn(Link, :sad_function, [])
+    receive do
+      msg ->
+        IO.puts("Message received: #{inspect(msg)}")
+      after 1000 ->
+        IO.puts("Nothing happened as far as I am concerned")
+    end
+  end
+end
+
+Link.run
