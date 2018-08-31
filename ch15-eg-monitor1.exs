93fbdd6622fedebe5e6d6b841504f928d5b4f468
Joao P Dubas <joao.dubas@gmail.com>
2018-08-30 23:30:48 -0300

    chapter 15: monitor: check process.l

---
+++ ch15-eg-monitor1.exs
@@ -0,0 +1,21 @@
+defmodule Monitor do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep(500)
+    exit(:boom)
+  end
+
+  def run do
+    res = spawn_monitor(Monitor, :sad_function, [])
+    IO.puts(inspect(res))
+    receive do
+      msg ->
+        IO.puts("Message received: #{inspect(msg)}")
+      after 1000 ->
+        IO.puts("Nothing happened as far as I'm concerned")
+    end
+  end
+end
+
+Monitor.run()
