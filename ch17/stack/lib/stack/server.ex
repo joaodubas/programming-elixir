0fc8144f39970040c7007c5b39fab8147ca935c4
Joao P Dubas <joao.dubas@gmail.com>
2018-09-01 07:14:59 -0300

    chapter 17: stack server: push items

--- ch17/stack/lib/stack/server.ex
+++ ch17/stack/lib/stack/server.ex
@@ -9,4 +9,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [h|t]) do
     {:reply, h, t}
   end
+
+  def handle_cast({:push, item}, current_value) do
+    {:noreply, [item|current_value]}
+  end
 end
