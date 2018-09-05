896c88902612ff7089eb62d9d60fdaccf67a9848
Joao P Dubas <joao.dubas@gmail.com>
2018-09-05 08:18:34 -0300

    chapter 18: Supervisor: manage app

---
+++ ch18/sequence/lib/sequence/server.ex
@@ -0,0 +1,45 @@
+defmodule Sequence.Server do
+  use GenServer
+
+  ###
+  # Public API
+
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call(__MODULE__, :next_number)
+  end
+
+  def set_number(new_number) do
+    GenServer.call(__MODULE__, {:set_number, new_number})
+  end
+
+  def increment_number(delta) do
+    GenServer.cast(__MODULE__, {:increment_number, delta})
+  end
+
+  ###
+  # GenServer callbacks
+
+  def init(initial_number) do
+    {:ok, initial_number}
+  end
+
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number + 1}
+  end
+
+  def handle_call({:set_number, new_number}, _from, _current_number) do
+    {:reply, new_number, new_number}
+  end
+
+  def handle_cast({:increment_number, delta}, current_number) do
+    {:noreply, current_number + delta}
+  end
+
+  def format_status(_reason, [_pdict, state]) do
+    [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
+  end
+end
