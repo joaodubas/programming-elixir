3a5b1f8c06b117488830ec165cdb7b891a68ed18
Joao P Dubas <joao.dubas@gmail.com>
2018-09-13 01:58:04 -0300

    chapter 19: dupper results

---
+++ ch19/duper/lib/duper/application.ex
@@ -0,0 +1,16 @@
+defmodule Duper.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      Duper.Results,
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
