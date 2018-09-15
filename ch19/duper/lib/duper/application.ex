93274be986b30213c7780261ea6c0177a3794e39
Joao P Dubas <joao.dubas@gmail.com>
2018-09-15 00:29:51 -0300

    chapter 19: duper worker sub-supervisor

--- ch19/duper/lib/duper/application.ex
+++ ch19/duper/lib/duper/application.ex
@@ -7,11 +7,12 @@ defmodule Duper.Application do
     children = [
       Duper.Results,
       {Duper.PathFinder, "."},
+      Duper.WorkerSupervisor,
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
